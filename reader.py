82a9bcce1a8fc5390324886e9a6398977f4d54a3
Gajan-lab <ict2023095@as.rjt.ac.lk>
2026-03-06 22:16:43 +0530

    Update reader.py

--- reader.py
+++ reader.py
@@ -3,7 +3,7 @@ import os
 
 def read_images():
 
-    folder_path = "frames"
+    folder_path = "frames" #please if the file not loaded correctly add frames folder path which is inculded with this file
 
     images = []
 
